6b900002cbf7328369188ed2564b5995b7bdf8fa
dmatavel <matavelidanilo@gmail.com>
2022-06-09 10:08:09 -0300

    hello, world

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+{
+    let nome = "Adão";
+    let numero = 73;
+    let ano = 2022;
+    let resultado = ano - numero;
+    let quartaFeira = true;
+    
+    console.log("o nome é: " + nome)
+    console.log("o número é: " + numero);
+    console.log("o ano é: " + ano)
+    console.log(`o resultado é: ${resultado}`);
+    console.log("o tipo é: " + typeof quartaFeira);
+    console.log("eu estou usando o console.log");
+}
