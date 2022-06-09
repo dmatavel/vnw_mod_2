256ffb9a4476af05ea0c56af14f5b46faefdbd8f
dmatavel <matavelidanilo@gmail.com>
2022-06-09 10:21:44 -0300

    english translation

--- challenge-01-data-structures/scripts/script.js
+++ challenge-01-data-structures/scripts/script.js
@@ -1,14 +1,14 @@
 {
-    let nome = "Adão";
-    let numero = 73;
-    let ano = 2022;
-    let resultado = ano - numero;
-    let quartaFeira = true;
+    let name = "Dan";
+    let number = 73;
+    let year = 2022;
+    let result = year - number;
+    let wednesday = true;
     
-    console.log("o nome é: " + nome)
-    console.log("o número é: " + numero);
-    console.log("o ano é: " + ano)
-    console.log(`o resultado é: ${resultado}`);
-    console.log("o tipo é: " + typeof quartaFeira);
-    console.log("eu estou usando o console.log");
+    console.log("my name is: " + name)
+    console.log("the number is: " + number);
+    console.log("the year is: " + year)
+    console.log(`the result is: ${result}`);
+    console.log("the data type is: " + typeof wednesday);
+    console.log("i'm using the console.log function");
 }
