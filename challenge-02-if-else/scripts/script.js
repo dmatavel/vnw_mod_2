26ae866d841bb2cbb5881d045a7cf9beb595cc9c
dmatavel <matavelidanilo@gmail.com>
2022-06-12 13:08:59 -0300

    normalized

--- challenge-02-if-else/scripts/script.js
+++ challenge-02-if-else/scripts/script.js
@@ -1,34 +1,27 @@
 let age;
+let isHuman;
+let birthDay;
+let firstName;
+let secondName;
 
 age = 18;
 if (age >= 18)
     console.log("Age is greater than or equal to 18.");
 else
     console.log("Age is lesser than 18.");
-
-let isHuman;
-
 isHuman = true;
 if (age >= 18 && isHuman)
     console.log("Congratulations! You are a human with more than 18 years.");
 else
     console.log("Try with another age or specie.");
-
-let birthDay;
 birthDay = "Janeiro";
 if (birthDay == "Janeiro" || birthDay == "Dezembro")
     console.log("Happy Birthday!");
-
-let firstName;
-
 firstName = "Rafael";
 if (firstName[0] == 'R')
     console.log("Your name starts with 'R'.");
 else
     console.log("Your name does not start with 'R'.");
-
-let secondName;
-
 secondName = "Michelangelo";
 for (i = 0; secondName[i]; i++)
 {
