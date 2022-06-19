f57146b946fbeb925bd91896f7fefed9975a9cc5
dmatavel <matavelidanilo@gmail.com>
2022-06-19 13:42:45 -0300

    challenge 04 done and minor updates

---
+++ 04_functions/scripts/script.js
@@ -0,0 +1,44 @@
+console.log("1) Implement a function that shows a message on the console.")
+
+function    helloWorld()
+{
+    console.log("Hello, World!");
+}
+
+helloWorld();
+
+console.log("2) Implement a function that receives a name as parameter and shows it on the console.")
+
+function    yourName(name)
+{
+    console.log(name);
+}
+
+yourName("Danilo");
+
+console.log("3) Implement a function that receives a name, an age and a favorite music genre as parameters and shows then on the console.")
+
+function    profile(name, age, favMusGen)
+{
+    console.log(name, age, favMusGen);
+}
+
+profile("Danilo,", "32,", "All.")
+
+console.log("4) Implement a function that receives a movie and a music title as parameters and shows then on the console.")
+
+function    works(movie, music)
+{
+    console.log(movie, music,);
+}
+
+works("Dreams,", "Paper Thin");
+
+console.log("5) Implement a function that returns three times the number received in the function parameter.")
+
+function    times3(x)
+{
+    return (x * 3);
+}
+
+console.log(times3(5));
